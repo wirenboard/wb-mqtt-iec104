2e95956172aad5a9dc42e16b8ebdeaa3941e7296
Nikita Maslov <nikita.maslov@wirenboard.com>
2024-08-21 13:47:44 +0500

    do not restart service on config file error

--- src/iec104_exception.cpp
+++ src/iec104_exception.cpp
@@ -2,3 +2,6 @@
 
 TEmptyConfigException::TEmptyConfigException(): std::runtime_error("Config file is empty")
 {}
+
+TConfigException::TConfigException(const std::string& message): std::runtime_error("Config error:" + message)
+{}
--- src/iec104_exception.h
+++ src/iec104_exception.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <stdexcept>
+#include <string>
+
+class TConfigException: public std::runtime_error
+{
+public:
+    explicit TConfigException(const std::string& message);
+};
 
 class TEmptyConfigException: public std::runtime_error
 {
